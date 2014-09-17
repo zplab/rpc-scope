b9ffc16ab9341694b063b4e8ac3ee54f756b739d
Zach Pincus <zpincus@gmail.com>
2014-09-16 23:20:34 -0500

    Add auto-property creation to RPC client namespace

--- simple_rpc.py
+++ simple_rpc.py
@@ -199,33 +199,73 @@ class RPCClient:
         """Use the RPC server's __DESCRIBE__ functionality to reconstitute a
         faxscimile namespace on the client side with well-described functions
         that can be seamlessly called."""
-        root = Namespace()
+        # group functions by their namespace
+        namespaces = collections.defaultdict(list)
         for qualname, doc, argspec in self('__DESCRIBE__'):
             *path, name = qualname.split('.')
-            namespace = _namespace_lookup_or_create(root, path)
-            rpc_func = self.proxy_function(qualname)
-            proxy_func = _rich_proxy_function(doc, argspec, name, rpc_func)
-            setattr(namespace, name, proxy_func)
-        self._add_proxy_properties(root)
+            namespaces[tuple(path)].append((name, qualname, doc, argspec))
+        
+        # for each namespace that contains functions:
+        # 1: see if there are any get/set pairs to turn into properties, and
+        # 2: make a class for that namespace with the given properties and functions
+        proxy_namespaces = {}
+        for path, function_descriptions in namespaces.items():
+            accessors = collections.defaultdict(RPCClient._accessor_pair)
+            functions = {}
+            for name, qualname, doc, argspec in function_descriptions:
+                rpc_func = self.proxy_function(qualname)
+                proxy_func = _rich_proxy_function(doc, argspec, name, rpc_func)
+                functions[name] = proxy_func
+                if name.startswith('get_'):
+                    accessors[name[4:]].getter = proxy_func
+                elif name.startswith('set_'):
+                    accessors[name[4:]].setter = proxy_func
+            # make a custom class to have the right names and more importantly to receive the namespace-specific properties
+            class LocalNamespace(Namespace):
+                pass
+            LocalNamespace.__name__ = path[-1]
+            LocalNamespace.__qualname__ = '.'.join(path)
+            for name, accessor_pair in accessors.items():
+                setattr(LocalNamespace, name, accessor_pair.get_property())
+            for name, proxy_func in functions.items():
+                setattr(LocalNamespace, name, proxy_func)
+            local_namespace = LocalNamespace()
+            proxy_namespaces[path] = local_namespace
+        
+        # now assemble these namespaces into the correct hierarchy, adding intermediate
+        # namespaces as required
+        root = proxy_namespaces.pop((), Namespace())
+        for path in sorted(proxy_namespaces.keys(), key=len):
+            # paths are sorted from less-deep to more-deep, so if the path isn't already in root, 
+            # we know we need to make a new Namespace() to hold it. If we didn't sort, we might
+            # get foo.bar from the proxy_namespaces dict first, and then get foo later, but we
+            # would have already made a new Namespace() foo to hold bar at that point.
+            *parent_path, name = path
+            parent_namespace = self._namespace_lookup_or_create(root, parent_path)
+            setattr(parent_namespace, name, proxy_namespaces[path])
         return root
-        
+    
     @staticmethod
-    def _add_proxy_properties(namespace):
-        accessors = collections.defaultdict(RPCClient._accessor_pair)
-        for k in dir(namespace):
-            if k.startswith('_'):
-                continue
-            v = getattr(namespace, k)
-            if callable(v) and not inspect.isclass(v):
-                if k.startswith('get_'):
-                    accessors[k[4:]].getter = v
-                elif k.startswith('set_'):
-                    accessors[k[4:]].setter = v
-            else:
-                RPCClient._add_proxy_properties(v)
-        for name, accessor_pair in accessors.items():
-            setattr(namespace, name, accessor_pair.get_property())
-        
+    def _namespace_lookup_or_create(namespace, path_elements):
+        """Find names nested in a namespace; if any elements of the name
+        are not present, create a dummy Namespace object with that name.
+
+        Example:
+        namespace = Namespace()
+        _namespace_lookup_or_create(namespace, ['foo', 'bar'])
+        namespace.foo.bar.baz = 6
+        _namespace_lookup_or_create(namespace, ['foo', 'quux'])
+        namespace.foo.quux.baz = 5    
+        """
+        for element in path_elements:
+            try:
+                namespace = getattr(namespace, element)
+            except AttributeError:
+                new_namespace = Namespace()
+                setattr(namespace, element, new_namespace)
+                namespace = new_namespace
+        return namespace
+            
     class _accessor_pair:
         def __init__(self):
             self.getter = None
@@ -254,26 +294,6 @@ class ZMQClient(RPCClient):
         return reply_dict['retval'], reply_dict['error']
 
 
-def _namespace_lookup_or_create(namespace, path_elements):
-    """Find names nested in a namespace; if any elements of the name
-    are not present, create a dummy Namespace object with that name.
-    
-    Example:
-    namespace = Namespace()
-    _namespace_lookup_or_create(namespace, ['foo', 'bar'])
-    namespace.foo.bar.baz = 6
-    _namespace_lookup_or_create(namespace, ['foo', 'quux'])
-    namespace.foo.quux.baz = 5    
-    """
-    for element in path_elements:
-        try:
-            namespace = getattr(namespace, element)
-        except AttributeError:
-            new_namespace = Namespace()
-            setattr(namespace, element, new_namespace)
-            namespace = new_namespace
-    return namespace
-
 def _rich_proxy_function(doc, argspec, name, to_proxy):
     """Using the docstring and argspec from the RPC __DESCRIBE__ command,
     generate a proxy function that looks just like the remote function, except
@@ -284,7 +304,9 @@ def _rich_proxy_function(doc, argspec, name, to_proxy):
     varkw = argspec['varkw']
     kwonly = argspec['kwonlyargs']
     kwdefaults = argspec['kwonlydefaults']
-    arg_parts = []
+    # note that the function we make has a "self" parameter as it is destined
+    # to be added to a class and used as a method.
+    arg_parts = ['self']
     call_parts = []
     # create the function by building up a python definition for that function
     # and exec-ing it.
@@ -315,11 +337,11 @@ def _rich_proxy_function(doc, argspec, name, to_proxy):
     # to generate closures correctly.
     func_str = '''
         def make_func(to_proxy):
-            def _({}):
+            def {}({}):
                 """{}"""
                 return to_proxy({})
-            return _
-    '''.format(', '.join(arg_parts), doc, ', '.join(call_parts))
+            return {}
+    '''.format(name, ', '.join(arg_parts), doc, ', '.join(call_parts), name)
     fake_locals = {} # dict in which exec operates: locals() doesn't work here.
     exec(func_str.strip(), globals(), fake_locals)
     func = fake_locals['make_func'](to_proxy) # call the factory function
