2e692c00273e9c00f6783242415090d9dafe0c6b
Erik Hvatum <ice.rikh@gmail.com>
2014-10-28 14:41:48 -0500

    Add Camera._AT_Enum class and example usage for review by Zach.

--- andor/camera.py
+++ andor/camera.py
@@ -26,16 +26,37 @@ import ctypes
 from rpc_acquisition.andor import andor
 
 class Camera:
-    '''Note: rpc_acquisition.andor.andor.initialize(..) should be called before instantiating this class.'''
-    def __init__(self):
+    '''This class provides an abstraction of the raw Andor API ctypes shim found in
+    rpc_acquisition.andor.andor.
+
+    Note that rpc_acquisition.andor.andor.initialize(..) should be called once before
+    instantiating this class.'''
+
+    class _AT_Enum:
+        def __init__(self, feature):
+            self._feature = feature
+            str_count = andor.GetEnumCount(self._feature)
+            self._idxs_to_strs = [andor.GetEnumStringByIndex(feature, idx) for idx in range(str_count)]
+            self._strs_to_idxs = {str_ : idx for idx, str_ in enumerate(self._idxs_to_strs)}
+
+        def str_to_idx(self, str_):
+            try:
+                return self._strs_to_idxs[str_]
+            except KeyError:
+                raise ValueError('Value for {} must be one of {}.'.format(self._feature, self._idxs_to_strs))
+
+        def idx_to_str(self, idx):
+            return self._idxs_to_strs[idx]
+
+    def __init__(self, property_server=None):
         self._property_server = None
+        if property_server is not None:
+            self._attach_property_server(property_server)
         # TODO: enumerate andor enum names and make name to enum id dict or python enum
+        self._AuxiliaryOutSource = self._AT_Enum('AuxiliaryOutSource')
 
-    def get_exposure_time(self):
-        return andor.GetFloat('ExposureTime')
-
-    def set_exposure_time(self, exposure_time):
-        andor.SetFloat('ExposureTime', exposure_time)
+    def __del__(self):
+        self._detach_property_server()
 
     def _attach_property_server(self, property_server):
         if self._property_server is not None:
@@ -43,7 +64,20 @@ class Camera:
         self._property_server = property_server
 
     def _detach_property_server(self):
-        if self._property_server is not None:
-            pass
+        if self._property_server is None:
+            return
 
 #   def _property_change_callback(feature, ):
+#       pass
+
+    def get_auxiliary_out_source(self):
+        return self._AuxiliaryOutSource.idx_to_str(andor.GetEnumIndex('AuxiliaryOutSource'))
+
+    def set_auxiliary_out_source(self, source):
+        andor.SetEnumIndex('AuxiliaryOutSource', self._AuxiliaryOutSource.str_to_idx(source))
+
+    def get_exposure_time(self):
+        return andor.GetFloat('ExposureTime')
+
+    def set_exposure_time(self, exposure_time):
+        andor.SetFloat('ExposureTime', exposure_time)
