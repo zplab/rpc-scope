2b5e498102b1c581c7371cf8e4c6b6a89ea93cf1
Zach Pincus <zpincus@gmail.com>
2014-12-11 11:16:14 -0600

    background autofocus

--- device/autofocus.py
+++ device/autofocus.py
@@ -25,6 +25,11 @@
 from wautofocuser import wautofocuser
 import numpy
 import time
+import scipy.signal as signal
+from concurrent import futures
+import threading
+import queue
+
 from ..util import transfer_ism_buffer
 
 def brenner(array, z):
@@ -36,12 +41,25 @@ _high_pass_filter = None
 def high_pass_brenner(array, z):
     global _high_pass_filter
     if _high_pass_filter is None or array.shape != (_high_pass_filter.w, _high_pass_filter.h):
-        _high_pass_filter = wautofocuser.Highpass(20, array.shape[0], array.shape[1])
+        _high_pass_filter = wautofocuser.Highpass(40, array.shape[0], array.shape[1])
     filtered_array = _high_pass_filter(array.astype(numpy.float32) / 65535)
     return brenner(filtered_array, z)
 
+# first parameter is order of the filter, second is the critical frequency as a fraction of nyquist
+# since the nyquist frequency is 2x the sampling rate, 2/n gives the frequency in terms of n spatial samples
+B, A = signal.butter(2, 2/40, 'highpass')
+
+def high_pass_brenner_lfilter(array, z):
+    filtered_x = signal.lfilter(B, A, array, axis=0)
+    filtered_y = signal.lfilter(B, A, array, axis=1)
+    x_diffs = (filtered_x[2:, :] - filtered_x[:-2, :])**2
+    y_diffs = (filtered_y[:, 2:] - filtered_y[:, :-2])**2
+    return x_diffs.sum() + y_diffs.sum()
+
+
 METRICS = {'brenner': brenner,
-           'high pass + brenner' : high_pass_brenner}
+           'high pass + brenner' : high_pass_brenner,
+           'lfilter high pass + brenner': high_pass_brenner_lfilter}
 
 class Autofocus:
     def __init__(self, camera, stage):
@@ -79,7 +97,7 @@ class Autofocus:
             self._stage.wait()
             return best_z, list(zip(z_positions, focus_metrics))
 
-    def autofocus_continuous_move(self, start, end, speed, metric='brenner', fps_max=None, ims=None):
+    def autofocus_continuous_move(self, start, end, speed, metric='brenner', fps_max=None, ims=None, max_workers=1):
         """Move the stage from 'start' to 'end' at a constant speed, taking images
         for autofocus constantly. If fps_max is None, take images as fast as
         possible; otherwise take images as governed by fps_max. Apply the autofocus
@@ -97,33 +115,64 @@ class Autofocus:
         # move the stage to the start position BEFORE we slow down the speed
         self._stage.set_z(start)
         self._stage.wait() # no op if in sync mode, necessary in async mode
+        evaluator = ImageEvaluator(metric, ims, max_workers)
         with self._stage._pushed_state(async=True, z_speed=speed):
             self._stage.wait()
-            z_positions = []
             self._stage.set_z(end)
             while self._stage.has_pending(): # while stage-move event is still in progress
                 self._camera.send_software_trigger()
                 # just queue the images up on the camera head while we do this
-                z_positions.append(self._stage.get_z())
+                evaluator.z_queue.put(self._stage.get_z())
                 time.sleep(sleep_time)
         self._stage.wait() # make sure all events are cleared out
-        focus_metrics = []
-        for z in z_positions:
-            print('getting image')
-            t0 = time.time()
-            name = self._camera.next_image(read_timeout_ms=1000)
-            print('got image {}'.format(time.time() - t0))
-            array = transfer_ism_buffer._release_array(name)
-            if ims is not None:
-                ims.append(array)
-            t1 = time.time()
-            focus_metrics.append(metric(array, z))
-            print('got metric {}'.format(time.time() - t1))
+        z_values, focus_metrics = evaluator.get_focus_values()
         # now that we've retrieved all the images, end the acquisition
         self._camera.end_image_sequence_acquisition()
         focus_order = numpy.argsort(focus_metrics)
-        best_z = z_positions[focus_order[-1]]
+        best_z = z_values[focus_order[-1]]
         self._stage.set_z(best_z) # go to focal plane with highest score
         self._stage.wait() # no op if in sync mode, necessary in async mode
-        return best_z, list(zip(z_positions, focus_metrics))
+        return best_z, list(zip(z_values, focus_metrics))
 
+def ImageEvaluator(Threading.thread):
+    def __init__(self, metric, ims, max_workers=1):
+        self.metric = metric
+        self.ims = ims
+        self.z_queue = queue.Queue()
+        self.executor = futures.ThreadPoolExecutor(max_workers)
+        self.running = True
+        self.focus_futures = []
+        self.z_values = []
+        super().__init__()
+        self.start()
+
+    def timer(self, array, z):
+        t = time.time()
+        value = metric(array, z)
+        print('got metric {}'.format(time.time() - t))
+        return value
+
+    def get_focus_values(self):
+        self.z_queue.join() # wait until enough task_done() calls are made to match the number of put() calls
+        self.running = False
+        self.join()
+        focus_metrics = [fut.result() for fut in self.focus_futures]
+        self.executor.shutdown()
+        return self.z_values, focus_metrics
+
+    def run(self):
+        while self.running:
+            try:
+                z_value = self.z_queue.get(timeout=0.25)
+            except queue.Empty:
+                continue
+            self.z_values.append(z_value)
+            print('getting image')
+            t = time.time()
+            name = self._camera.next_image(read_timeout_ms=1000)
+            array = transfer_ism_buffer._release_array(name)
+            if self.ims is not None:
+                self.ims.append(array)
+            print('got image {}'.format(time.time() - t))
+            self.focus_futures.append(self.executor.submit(self.timer, (array, z)))
+            self.z_queue.task_done()
