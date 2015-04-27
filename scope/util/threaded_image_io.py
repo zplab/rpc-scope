921a55d6e8166773546a2a7d04c5e1b7ec5925b5
Zach Pincus <zpincus@gmail.com>
2015-04-26 23:43:43 -0500

    fix image io swalling errors

--- scope/util/threaded_image_io.py
+++ scope/util/threaded_image_io.py
@@ -8,7 +8,13 @@ class ThreadedIO:
     def write(self, images, paths, flags=0):
         """Write out a list of images to the given paths."""
         futures_out = [self.threadpool.submit(freeimage.write, image, str(path), flags) for image, path in zip(images, paths)]
+        # wait until all have completed or errored out
         futures.wait(futures_out)
+        # now get the result() from each future, which will raise any errors encountered
+        # during the execution.
+        # The futures.wait() call above makes sure that everything that doesn't
+        # error out has a chance to finish before we barf an exception.
+        [f.result() for f in futures_out]
 
     def read(self, paths):
         """Return an iterator over image arrays read from the given paths."""
