a4747545474d5c6fca3ea7bd271bea6ad3c53e36
Erik Hvatum <ice.rikh@gmail.com>
2015-09-25 16:26:00 -0500

    Continue implementing MicroscopeWidget and add it to the things made by scope_gui.py.

--- scope/cli/scope_gui.py
+++ scope/cli/scope_gui.py
@@ -3,20 +3,22 @@ import argparse
 from ..gui import andor_camera_widget
 from ..gui import lamp_widget
 from ..gui import live_viewer_widget
+from ..gui import microscope_widget
 from ..gui import build_gui
 
 WIDGETS = {
     'camera': andor_camera_widget.AndorCameraWidget,
     'lamps': lamp_widget.LampWidget,
-    'viewer': live_viewer_widget.LiveViewerWidget
+    'viewer': live_viewer_widget.LiveViewerWidget,
+    'microscope': microscope_widget.MicroscopeWidget
 }
 
 def main(argv):
     parser = argparse.ArgumentParser(description="microscope GUI")
-    parser.add_argument('widgets', nargs="*", choices=['camera', 'viewer', 'lamps', 'all'], default='all',
+    parser.add_argument('widgets', nargs="*", choices=['camera', 'viewer', 'lamps', 'microscope', 'all'], default='all',
             help='the widget(s) to show %(prog)s (default: %(default)s)')
     parser.add_argument('--host', default='127.0.0.1', help='microscope host to connect to (default %(default)s')
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
     if not isinstance(args.widgets, list):
         args.widgets = [args.widgets]
     widgets = set(args.widgets)
--- scope/gui/microscope_widget.py
+++ scope/gui/microscope_widget.py
@@ -22,8 +22,95 @@
 #
 # Authors: Erik Hvatum <ice.rikh@gmail.com>
 
-from . import device_widget
+import enum
 from PyQt5 import Qt
+from . import device_widget
+
+class PT(enum.Enum):
+    Bool = 0,
+    Int = 1,
+    Float = 2,
+    Enum = 3,
+    IdxEnum = 4
 
 class MicroscopeWidget(device_widget.DeviceWidget):
     PROPERTY_ROOT = 'scope.'
+    PROPERTIES = [
+        ('stand.active_microscopy_method', PT.Enum, 'stand.available_microscopy_methods'),
+        ('nosepiece.position', PT.IdxEnum, 'nosepiece.all_objectives'),
+        ('nosepiece.safe_mode', PT.Bool),
+        ('nosepiece.immersion_mode', PT.Bool),
+        ('il.shutter_open', PT.Bool),
+        ('tl.shutter_open', PT.Bool),
+        ('il.field_wheel', PT.Enum, 'il.field_wheel_positions'),
+        ('il.filter_cube', PT.Enum, 'il.filter_cube_values'),
+        ('tl.aperture_diaphragm', PT.Int),
+        ('tl.field_diaphragm', PT.Int),
+        ('tl.condenser_retracted', PT.Bool),
+        ('stage.x', PT.Float),
+        ('stage.y', PT.Float),
+        ('stage.z', PT.Float)]
+
+    @classmethod
+    def can_run(cls, scope):
+        # We're good if at least one of our properties can be read.  Properties that can not be read
+        # when the widget is created are not shown in the GUI.
+        for ppath, *_ in prop_tuple in cls.PROPERTIES:
+            C = scope
+            ppathcs = ppath.split('.')
+            try:
+                for ppathc in ppathcs:
+                    C = getattr(C, ppathc)
+            except AttributeError:
+                continue
+            return True
+        return False
+
+    def __init__(self, scope, scope_properties, parent=None):
+        super().__init__(scope, scope_properties, parent)
+        self.setWindowTitle('Microscope')
+        self.setLayout(Qt.QGridLayout())
+        self.scope = scope
+        self.widget_makers = {
+            PT.Bool : self.make_bool_widget,
+            PT.Int : self.make_numeric_widget,
+            PT.Float : self.make_numeric_widget,
+            PT.Enum : self.make_enum_widget,
+            PT.IdxEnum : self.make_idx_enum_widget}
+        for ptuple in self.PROPERTIES:
+            self.make_widgets_for_property(ptuple)
+
+    def make_widgets_for_property(self, ptuple):
+        if ptuple[1] is not PT.Bool:
+            return
+        layout = self.layout()
+        row = layout.rowCount()
+        label = Qt.QLabel(ptuple[0] + ':')
+        layout.addWidget(label, row, 0)
+        widget = self.widget_makers[ptuple[1]](ptuple)
+        layout.addWidget(widget, row, 1)
+        return label, widget
+
+    def make_bool_widget(self, ptuple):
+        widget = Qt.QCheckBox()
+        update = self.subscribe(self.PROPERTY_ROOT + ptuple[0], callback=widget.setChecked)
+        print(self.PROPERTY_ROOT + ptuple[0])
+        if update is None:
+            raise TypeError('{} is not a writable property!'.format(property))
+        def changed(value):
+            try:
+                update(value)
+            except rpc_client.RPCError as e:
+                error = 'Could not set {} ({}).'.format(property, e.args[0])
+                Qt.QMessageBox.warning(self, 'Invalid Value', error)
+        widget.toggled.connect(changed)
+        return widget
+
+    def make_numeric_widget(self, ptuple):
+        pass
+
+    def make_enum_widget(self, ptuple):
+        pass
+
+    def make_idx_enum_widget(self, ptuple):
+        pass
