4ebd88bad435271a5fde39394f98d2289cdb36c3
Erik Hvatum <ice.rikh@gmail.com>
2016-05-25 15:47:18 -0500

    Make and use "Live Target" Layer in ScopeViewer. Supply current imposed_image_mask as mask argument to newly instantiated live Image instances, avoiding computation of never-used maskless ndimage statistics.

--- scope/gui/scope_viewer.py
+++ scope/gui/scope_viewer.py
@@ -23,11 +23,18 @@
 # Authors: Erik Hvatum <ice.rikh@gmail.com>
 
 from PyQt5 import Qt
-from ris_widget import ris_widget
-from ris_widget import image as ris_widget_image
+import ris_widget
+import ris_widget.image
+import ris_widget.layer
+import ris_widget.ris_widget
+import ris_widget.qwidgets.layer_table
 from .. import scope_client
 
-class ScopeViewerQtObject(ris_widget.RisWidgetQtObject):
+# Show layer name column in LayerTable
+ris_widget.qwidgets.layer_table.LayerTableModel.PROPERTIES.insert(
+    ris_widget.qwidgets.layer_table.LayerTableModel.PROPERTIES.index('opacity') + 1, 'name')
+
+class ScopeViewerQtObject(ris_widget.ris_widget.RisWidgetQtObject):
     RW_LIVE_STREAM_BINDING_LIVE_UPDATE_EVENT = Qt.QEvent.registerEventType()
 
     def __init__(
@@ -56,12 +63,24 @@ class ScopeViewerQtObject(ris_widget.RisWidgetQtObject):
         self.scope = scope
         self.scope_toolbar = self.addToolBar('Scope')
         self.live_streamer = scope_client.LiveStreamer(scope, scope_properties, self.post_live_update)
+        self.get_live_target_layer()
 
     def event(self, e):
         # This is called by the main QT event loop to service the event posted in post_live_update().
         if e.type() == self.RW_LIVE_STREAM_BINDING_LIVE_UPDATE_EVENT:
             image_data, timestamp, frame_no = self.live_streamer.get_image()
-            self.image = ris_widget_image.Image(image_data, is_twelve_bit=self.live_streamer.bit_depth=='12 Bit')
+            target_layer = self.get_live_target_layer()
+            is_twelve_bit = self.live_streamer.bit_depth == '12 Bit'
+            if target_layer.image is None:
+                target_layer.image = ris_widget.image.Image(
+                    image_data,
+                    mask=self.layer_stack.imposed_image_mask,
+                    is_twelve_bit=is_twelve_bit)
+            else:
+                target_layer.image.set(
+                    image_data,
+                    mask=self.layer_stack.imposed_image_mask,
+                    is_twelve_bit=is_twelve_bit)
             return True
         return super().event(e)
 
@@ -70,9 +89,22 @@ class ScopeViewerQtObject(ris_widget.RisWidgetQtObject):
         # unlike sending a signal
         Qt.QCoreApplication.postEvent(self, Qt.QEvent(self.RW_LIVE_STREAM_BINDING_LIVE_UPDATE_EVENT))
 
-class ScopeViewer(ris_widget.RisWidget):
+    def get_live_target_layer(self):
+        """The first Layer in self.layers with name "Live Target" is returned.  If self.layers contains no Layer with name
+        "Live Target", one is created, inserted at index 0, and returned."""
+        if self.layers is None:
+            self.layers = []
+        else:
+            for layer in self.layers:
+                if layer.name == 'Live Target':
+                    return layer
+        t = ris_widget.layer.Layer(name='Live Target')
+        self.layers.insert(0, t)
+        return t
+
+class ScopeViewer(ris_widget.ris_widget.RisWidget):
     APP_PREFS_NAME = "ScopeViewer"
-    COPY_REFS = ris_widget.RisWidget.COPY_REFS + [
+    COPY_REFS = ris_widget.ris_widget.RisWidget.COPY_REFS + [
         #'something'
     ]
     QT_OBJECT_CLASS = ScopeViewerQtObject
