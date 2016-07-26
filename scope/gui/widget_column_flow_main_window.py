f720716ec04031fd1e6fadf8b3d4daba8bbd8fcd
Erik Hvatum <ice.rikh@gmail.com>
2016-07-26 16:22:47 -0500

    Put contained widgets in floatable container with float button at the top.

--- scope/gui/widget_column_flow_main_window.py
+++ scope/gui/widget_column_flow_main_window.py
@@ -31,16 +31,49 @@ class WidgetColumnFlowMainWindow(Qt.QMainWindow):
         self._w = Qt.QWidget()
         self._w.setLayout(Qt.QHBoxLayout())
         self.setCentralWidget(self._w)
+        self.widgets_to_containers = {}
+        self.containers_to_widgets = {}
 
     def add_widget(self, widget):
         assert widget not in self.widgets
+        container = FloatableWidgetContainer(widget)
+        self.widgets_to_containers[widget] = container
+        self.containers_to_widgets[container] = widget
         self.widgets.append(widget)
-        self._w.layout().addWidget(widget)
+        self._w.layout().addWidget(container)
 
     def remove_widget(self, widget):
         assert widget in self.widgets
+        container = self.widgets_to_containers[widget]
+        del self.widgets_to_containers[widget]
+        del self.containers_to_widgets[container]
         del self.widgets[self.widgets.index(widget)]
-        self._w.layout().removeWidget(widget)
+        self._w.layout().removeWidget(container)
 
     def reflow(self):
         pass
+
+class FloatableWidgetContainer(Qt.QWidget):
+    pop_request_signal = Qt.pyqtSignal(Qt.QWidget, bool)
+
+    def __init__(self, contained_widget):
+        super().__init__()
+        self.contained_widget = contained_widget
+        l = Qt.QVBoxLayout()
+        self.setLayout(l)
+        self.pop_groupbox = Qt.QGroupBox()
+        l.addWidget(self.pop_groupbox)
+        ll = Qt.QHBoxLayout()
+        ll.setContentsMargins(0,0,0,0)
+        self.pop_groupbox.setLayout(ll)
+        ll.addSpacerItem(Qt.QSpacerItem(0, 0, Qt.QSizePolicy.Expanding))
+        self.pop_button = Qt.QPushButton('\N{NORTH EAST ARROW}')
+        self.pop_button.setCheckable(True)
+        self.pop_button.setChecked(False)
+        self.pop_button.clicked.connect(self.on_popout_button_clicked)
+        ll.addWidget(self.pop_button)
+        l.addWidget(contained_widget)
+        l.addSpacerItem(Qt.QSpacerItem(0, 0, Qt.QSizePolicy.Expanding, Qt.QSizePolicy.Expanding))
+
+    def on_popout_button_clicked(self, out):
+        self.pop_request_signal.emit(self, out)
