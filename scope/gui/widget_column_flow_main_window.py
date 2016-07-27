1b79e03cf13d230815a422ce39ce8d8091621f63
Erik Hvatum <ice.rikh@gmail.com>
2016-07-27 17:33:55 -0500

    Implement horizontal reflowing.

--- scope/gui/widget_column_flow_main_window.py
+++ scope/gui/widget_column_flow_main_window.py
@@ -29,7 +29,7 @@ class WidgetColumnFlowMainWindow(Qt.QMainWindow):
         super().__init__(parent)
         self.widgets = []
         self._w = Qt.QWidget()
-        self._w.setLayout(Qt.QHBoxLayout())
+        self._w.setLayout(FlowLayout())
         self.setCentralWidget(self._w)
         self.widgets_to_containers = {}
         self.visibility_toolbar = self.addToolBar('Visibility')
@@ -158,3 +158,102 @@ class FloatableHideableWidgetContainer(Qt.QWidget):
     @property
     def is_visible(self):
         return self.visibility_change_action.isChecked()
+
+# Adapted from Qt Docs / Qt Widgets / Flow Layout Example
+class FlowLayout(Qt.QLayout):
+    def __init__(self, margin=-1, hSpacing=-1, vSpacing=-1):
+        super().__init__()
+        self.m_hSpace = hSpacing
+        self.m_vSpace = vSpacing
+        self.setContentsMargins(margin, margin, margin, margin)
+        self.itemList = []
+
+    def addItem(self, item):
+        self.itemList.append(item)
+
+    def horizontalSpacing(self):
+        if self.m_hSpace >= 0:
+            return self.m_hSpace
+        else:
+            return self.smartSpacing(Qt.QStyle.PM_LayoutHorizontalSpacing)
+
+    def verticalSpacing(self):
+        if self.m_vSpace >= 0:
+            return self.m_vSpace
+        else:
+            return self.smartSpacing(Qt.QStyle.PM_LayoutVerticalSpacing)
+
+    def count(self):
+        return len(self.itemList)
+
+    def itemAt(self, index):
+        if 0 <= index < len(self.itemList):
+            return self.itemList[index]
+
+    def takeAt(self, index):
+        if 0 <= index < len(self.itemList):
+            v = self.itemList[index]
+            del self.itemList[index]
+
+    def expandingDirections(self):
+        return 0
+
+    def hasHeightForWidth(self):
+        return True
+
+    def heightForWidth(self, width):
+        height = self.doLayout(Qt.QRect(0, 0, width, 0), True);
+        return height
+
+    def setGeometry(self, rect):
+        super().setGeometry(rect)
+        self.doLayout(rect, False)
+
+    def sizeHint(self):
+        return self.minimumSize()
+
+    def minimumSize(self):
+      size = Qt.QSize()
+      for item in self.itemList:
+          size = size.expandedTo(item.minimumSize())
+      size += Qt.QSize(2*self.spacing(), 2*self.spacing())
+      return size
+
+    def doLayout(self, rect, testOnly):
+        left, top, right, bottom = self.getContentsMargins()
+        effectiveRect = rect.adjusted(+left, +top, -right, -bottom)
+        x = effectiveRect.x()
+        y = effectiveRect.y()
+        lineHeight = 0
+
+        for item in self.itemList:
+            wid = item.widget()
+            spaceX = self.horizontalSpacing()
+            if spaceX == -1:
+                spaceX = wid.style().layoutSpacing(Qt.QSizePolicy.PushButton, Qt.QSizePolicy.PushButton, Qt.Qt.Horizontal)
+            spaceY = self.verticalSpacing()
+            if spaceY == -1:
+                spaceY = wid.style().layoutSpacing(Qt.QSizePolicy.PushButton, Qt.QSizePolicy.PushButton, Qt.Qt.Vertical)
+            nextX = x + item.sizeHint().width() + spaceX
+            if nextX - spaceX > effectiveRect.right() and lineHeight > 0:
+                x = effectiveRect.x()
+                y = y + lineHeight + spaceY
+                nextX = x + item.sizeHint().width() + spaceX
+                lineHeight = 0
+
+            if not testOnly:
+                item.setGeometry(Qt.QRect(Qt.QPoint(x, y), item.sizeHint()))
+
+            x = nextX
+            lineHeight = max(lineHeight, item.sizeHint().height())
+
+        return y + lineHeight + rect.y() + bottom
+
+    def smartSpacing(self, pm):
+        parent = self.parent()
+        if parent is None:
+            return -1
+        elif parent.isWidgetType():
+          return parent.style().pixelMetric(pm, None, parent)
+        else:
+            return parent.spacing()
