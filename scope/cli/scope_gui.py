c03f5165eb29527b01ac80242f7b506c78e9dde0
Erik Hvatum <ice.rikh@gmail.com>
2016-07-29 15:18:34 -0500

    Include list of widget names from scope.gui.scope_widgets.WIDGETS in scope.cli.scope_gui.__main__() help info rather than using hardcoded list.

--- scope/cli/scope_gui.py
+++ scope/cli/scope_gui.py
@@ -1,11 +1,15 @@
 import argparse
 
 from ..gui import build_gui
+from ..gui import scope_widgets
 
 def main(argv):
     parser = argparse.ArgumentParser(description="microscope GUI")
-    parser.add_argument('widgets', nargs="*", choices=['camera', 'viewer', 'lamps', 'microscope', 'all'], default='all',
-            help='the widget(s) to show %(prog)s (default: %(default)s)')
+    choices = sorted(scope_widgets.WIDGETS.keys())
+    choices.append('all')
+    parser.add_argument(
+        'widgets', nargs="*", choices=choices, default='all',
+        help='the widget(s) to show %(prog)s (default: %(default)s)')
     parser.add_argument('--host', default='127.0.0.1', help='microscope host to connect to (default %(default)s')
     args = parser.parse_args(argv)
     if not isinstance(args.widgets, list):
