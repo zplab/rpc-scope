16b24695adb08bcb098ae9ae4e77f2015bbca4d8
Zach Pincus <zpincus@gmail.com>
2017-01-17 15:31:57 -0600

    fix missing files from pip install

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-import distutils.core
+import setuptools
 
-distutils.core.setup(name='scope',
-    version='1.0',
-    description='zplab microscope package',
-    packages=['scope', 'scope.cli', 'scope.client_util', 'scope.config',
-        'scope.device',  'scope.device.andor', 'scope.device.iotool',
-        'scope.device.leica', 'scope.gui', 'scope.messaging', 'scope.simple_rpc',
-        'scope.timecourse', 'scope.util'],
-    scripts=['scripts/scope_gui', 'scripts/scope_job_runner', 'scripts/scope_server']
-    )
+setuptools.setup(
+    name = 'scope',
+    version = '1.5',
+    description = 'zplab microscope package',
+    packages = setuptools.find_packages(),
+    package_data = {'scope.gui':['limit_icons/*.svg']},
+    scripts = ['scripts/scope_gui', 'scripts/scope_job_runner', 'scripts/scope_server']
+)
