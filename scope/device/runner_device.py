e7a94e99df8516d429103f9fb37a1945a0d0d5b5
Zach Pincus <zpincus@gmail.com>
2018-04-17 11:07:27 -0500

    actually add files

---
+++ scope/device/runner_device.py
@@ -0,0 +1,52 @@
+# This code is licensed under the MIT License (see LICENSE file for details)
+
+from ..util import property_device
+from ..util import timer
+from .. import scope_job_runner
+
+class JobRunner(property_device.PropertyDevice):
+    _DESCRIPTION = 'job runner'
+
+    def __init__(self, property_server=None, property_prefix=''):
+        self.runner = scope_job_runner.JobRunner()
+        if property_server:
+            self._fast_timer_thread = timer.Timer(self._update_fast_properties, interval=60) # every minute
+            self._slow_timer_thread = timer.Timer(self._update_slow_properties, interval=3600) # every hour
+
+    def _update_fast_properties(self):
+        self.get_running()
+        self.get_jobs()
+        self.get_current_job()
+
+    def _update_slow_properties(self):
+        self.get_duty_cycle()
+
+    def get_jobs(self):
+        # don't return incubator_check job...
+        jobs = [job for job in self.runner.jobs.get_jobs() if job.exec_file.name != 'incubator_check']
+        statuses = [job.status for job in jobs]
+        self._update_property('queued_jobs' statuses.count(scope_job_runner.STATUS_QUEUED))
+        self._update_property('errored_jobs', statuses.count(scope_job_runner.STATUS_ERROR))
+        jobs_out = []
+        for job in jobs:
+            d = job._asdict()
+            d['exec_file'] = str(d['exec_file'])
+            jobs_out.append(d)
+        return jobs_out
+
+    def get_current_job(self):
+        job = self.runner.current_job.get()
+        if job is not None:
+            job = str(job)
+        self._update_property('current_job', job)
+        return job
+
+    def get_running(self):
+        running = self.runner.is_running()
+        self._update_property('running', running)
+        return running
+
+    def get_duty_cycle(self):
+        job_hours, duty_cycle = self.runner.duty_cycles(intervals=[24])[0]
+        self._update_property('duty_cycle', duty_cycle)
+        return duty_cycle
---
+++ scope/gui/status_widget.py
@@ -0,0 +1,67 @@
+# This code is licensed under the MIT License (see LICENSE file for details)
+
+import pathlib
+from PyQt5 import Qt
+
+from . import device_widget
+
+class StatusWidget(device_widget.DeviceWidget):
+    @staticmethod
+    def can_run(scope):
+        return True
+
+    PROPERTY_DEFAULTS = dict(running=False, queued_jobs=0, errored_jobs=0, duty_cycle=0, current_job=None)
+
+    def __init__(self, scope, scope_properties, parent=None):
+        super().__init__(scope, scope_properties, parent)
+        self.setWindowTitle('Status')
+        layout = Qt.QHBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(10)
+        self.server_label = Qt.QLabel()
+        self.runner_label = Qt.QLabel()
+        layout.addWidget(self.server_label)
+        layout.addWidget(self.runner_label)
+        self.current_label = Qt.QLabel()
+        layout.addWidget(self.current_label)
+        layout.addStretch()
+
+        if device_widget.has_component(scope, 'scope.job_runner'):
+            for name, default in self.PROPERTY_DEFAULTS.items():
+                setattr(self, name, default)
+                self.subscribe('scope.job_runner.'+prop, lambda value: self.update(name, value))
+
+        self.server_running = False
+        self.timerEvent()
+        self.startTimer(60*1000, Qt.Qt.VeryCoarseTimer) # run timerEvent every 60 sec
+
+    def update(self, name=None, value=None):
+        if name is not None:
+            setattr(self, name, value)
+        _label_text(self.server_label, 'Server', self.server_running)
+        suffix = f'{self.duty_cycle}% utilization; {self.queued_jobs} jobs queued'
+        if self.errored_jobs > 0:
+            suffix +=f'; <span style="font-weight: bold; color: red">{self.errored_jobs} job errors</span>'
+        _label_text(self.runner_label, 'Job Runner', self.running, suffix)
+        if self.current_job is None:
+            self.current_label.setText('No job running.')
+        else:
+            job = pathlib.Path(self.current_job)
+            self.current_label.setText(f'Running job "{job.parent.parent.name}/{job.parent.name}".')
+
+    def timerEvent(self, event):
+        try:
+            self.scope._sleep(0) # no op to see if server is responding
+            running = True
+        except:
+            running = False
+        self.update('server_running', running)
+
+def _label_text(label, prefix, status_ok=True, suffix=None):
+    color = 'green' if status_ok else 'red'
+    dot = f'<span style="color: {color}">\N{BLACK CIRCLE}</span>'
+    if suffix is not None:
+        suffix = f' ({suffix})'
+    else:
+        suffix = ''
+    label.setText(f'{prefix}: {dot}{suffix}')
